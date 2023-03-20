8b609c445f9371e1fd044f6b6cbd8e8cc4e30d1e
Alp Soysal <alpbsoysal@gmail.com>
2023-03-20 12:50:13 +0000

    Bug fixes to get loop version working

--- ShallowWater.cpp
+++ ShallowWater.cpp
@@ -19,6 +19,21 @@ namespace po = boost::program_options;
 
 constexpr double G = 9.81;
 
+void PrintMatrix(int n, int m, double* H) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << setw(10) << H[j*n+i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void PrintVector(int n, double* u) {
+    for (int i = 0; i < n; ++i) {
+        cout << u[i] << endl;
+    }
+}
+
 /**
  * @class ShallowWater
  * @brief Implements a numerical solution to the shallow water equations.
@@ -345,19 +360,19 @@ void ShallowWater::deri_x(const double* var, double* der) {
     for (int row = 0; row < ny; row++)
     {
         // first 3 elements in row
-        der[0*ny + row] = (-1/60*var[(nx-3)*ny+row] + 3/20*var[(nx-2)*ny+row] - 3/4*var[(nx-1)*ny+row] + 3/4*var[ny+row] - 3/20*var[2*ny+row] + 1/60*var[3*ny+row])/dx;
-        der[1*ny + row] = (-1/60*var[(nx-2)*ny+row] + 3/20*var[(nx-1)*ny+row] - 3/4*var[row] + 3/4*var[2*ny+row] - 3/20*var[3*ny+row] + 1/60*var[4*ny+row])/dx;
-        der[2*ny + row] = (-1/60*var[(nx-1)*ny+row] + 3/20*var[row] - 3/4*var[ny+row] + 3/4*var[3*ny+row] - 3/20*var[4*ny+row] + 1/60*var[5*ny+row])/dx;
+        der[0*ny + row] = (-1.0/60.0*var[(nx-3)*ny+row] + 3.0/20.0*var[(nx-2)*ny+row] - 3.0/4.0*var[(nx-1)*ny+row] + 3.0/4.0*var[ny+row] - 3.0/20.0*var[2*ny+row] + 1.0/60.0*var[3*ny+row])/dx;
+        der[1*ny + row] = (-1.0/60.0*var[(nx-2)*ny+row] + 3.0/20.0*var[(nx-1)*ny+row] - 3.0/4.0*var[row] + 3.0/4.0*var[2*ny+row] - 3.0/20.0*var[3*ny+row] + 1.0/60.0*var[4*ny+row])/dx;
+        der[2*ny + row] = (-1.0/60.0*var[(nx-1)*ny+row] + 3.0/20.0*var[row] - 3.0/4.0*var[ny+row] + 3.0/4.0*var[3*ny+row] - 3.0/20.0*var[4*ny+row] + 1.0/60.0*var[5*ny+row])/dx;
 
         // last 3 elements in row
-        der[(nx-3)*ny + row] = (-1/60*var[(nx-6)*ny+row] + 3/20*var[(nx-5)*ny+row] - 3/4*var[(nx-4)*ny+row] + 3/4*var[(nx-2)*ny+row] - 3/20*var[(nx-1)*ny+row] + 1/60*var[row])/dx;
-        der[(nx-2)*ny + row] = (-1/60*var[(nx-5)*ny+row] + 3/20*var[(nx-4)*ny+row] - 3/4*var[(nx-3)*ny+row] + 3/4*var[(nx-1)*ny+row] - 3/20*var[row] + 1/60*var[ny+row])/dx;
-        der[(nx-1)*ny + row] = (-1/60*var[(nx-4)*ny+row] + 3/20*var[(nx-3)*ny+row] - 3/4*var[(nx-2)*ny+row] + 3/4*var[row] - 3/20*var[ny+row] + 1/60*var[2*ny+row])/dx;
+        der[(nx-3)*ny + row] = (-1.0/60.0*var[(nx-6)*ny+row] + 3.0/20.0*var[(nx-5)*ny+row] - 3.0/4.0*var[(nx-4)*ny+row] + 3.0/4.0*var[(nx-2)*ny+row] - 3.0/20.0*var[(nx-1)*ny+row] + 1.0/60.0*var[row])/dx;
+        der[(nx-2)*ny + row] = (-1.0/60.0*var[(nx-5)*ny+row] + 3.0/20.0*var[(nx-4)*ny+row] - 3.0/4.0*var[(nx-3)*ny+row] + 3.0/4.0*var[(nx-1)*ny+row] - 3.0/20.0*var[row] + 1.0/60.0*var[ny+row])/dx;
+        der[(nx-1)*ny + row] = (-1.0/60.0*var[(nx-4)*ny+row] + 3.0/20.0*var[(nx-3)*ny+row] - 3.0/4.0*var[(nx-2)*ny+row] + 3.0/4.0*var[row] - 3.0/20.0*var[ny+row] + 1.0/60.0*var[2*ny+row])/dx;
 
         // elements between first 3 and last 3 elements
         for (int col = 3; col < nx-3; col++)
         {
-            der[col*ny + row] = (-1/60*var[(col-3)*ny+row] + 3/20*var[(col-2)*ny+row] - 3/4*var[(col-1)*ny+row] + 3/4*var[(col+1)*ny+row] - 3/20*var[(col+2)*ny+row] + 1/60*var[(col+3)*ny+row])/dx;
+            der[col*ny + row] = (-1.0/60.0*var[(col-3)*ny+row] + 3.0/20.0*var[(col-2)*ny+row] - 3.0/4.0*var[(col-1)*ny+row] + 3.0/4.0*var[(col+1)*ny+row] - 3.0/20.0*var[(col+2)*ny+row] + 1.0/60.0*var[(col+3)*ny+row])/dx;
         }
     }
 }
@@ -373,19 +388,19 @@ void ShallowWater::deri_y(const double* var, double* der) {
     for (int col = 0; col < nx; col++)
     {
         // first 3 elements in row
-        der[col*ny + 0] = (-1/60*var[col*ny+ny-3] + 3/20*var[col*ny+ny-2] - 3/4*var[col*ny+ny-1] + 3/4*var[col*ny+1] - 3/20*var[col*ny+2] + 1/60*var[col*ny+3])/dy;
-        der[col*ny + 1] = (-1/60*var[col*ny+ny-2] + 3/20*var[col*ny+ny-1] - 3/4*var[col*ny] + 3/4*var[col*ny+2] - 3/20*var[col*ny+3] + 1/60*var[col*ny+4])/dy;
-        der[col*ny + 2] = (-1/60*var[col*ny+ny-1] + 3/20*var[col*ny] - 3/4*var[col*ny+1] + 3/4*var[col*ny+3] - 3/20*var[col*ny+4] + 1/60*var[col*ny+5])/dy;
+        der[col*ny + 0] = (-1.0/60.0*var[col*ny+ny-3] + 3.0/20.0*var[col*ny+ny-2] - 3.0/4.0*var[col*ny+ny-1] + 3.0/4.0*var[col*ny+1] - 3.0/20.0*var[col*ny+2] + 1.0/60.0*var[col*ny+3])/dy;
+        der[col*ny + 1] = (-1.0/60.0*var[col*ny+ny-2] + 3.0/20.0*var[col*ny+ny-1] - 3.0/4.0*var[col*ny] + 3.0/4.0*var[col*ny+2] - 3.0/20.0*var[col*ny+3] + 1.0/60.0*var[col*ny+4])/dy;
+        der[col*ny + 2] = (-1.0/60.0*var[col*ny+ny-1] + 3.0/20.0*var[col*ny] - 3.0/4.0*var[col*ny+1] + 3.0/4.0*var[col*ny+3] - 3.0/20.0*var[col*ny+4] + 1.0/60.0*var[col*ny+5])/dy;
 
         // last 3 elements in row
-        der[col*ny + ny-3] = (-1/60*var[col*ny+ny-6] + 3/20*var[col*ny+ny-5] - 3/4*var[col*ny+ny-4] + 3/4*var[col*ny+ny-2] - 3/20*var[col*ny+ny-1] + 1/60*var[col*ny])/dy;
-        der[col*ny + ny-2] = (-1/60*var[col*ny+ny-5] + 3/20*var[col*ny+ny-4] - 3/4*var[col*ny+ny-3] + 3/4*var[col*ny+ny-1] - 3/20*var[col*ny] + 1/60*var[col*ny+1])/dy;
-        der[col*ny + ny-1] = (-1/60*var[col*ny+ny-4] + 3/20*var[col*ny+ny-3] - 3/4*var[col*ny+ny-2] + 3/4*var[col*ny] - 3/20*var[col*ny+1] + 1/60*var[col*ny+2])/dy;
+        der[col*ny + ny-3] = (-1.0/60*var[col*ny+ny-6] + 3.0/20.0*var[col*ny+ny-5] - 3.0/4.0*var[col*ny+ny-4] + 3.0/4.0*var[col*ny+ny-2] - 3.0/20.0*var[col*ny+ny-1] + 1.0/60.0*var[col*ny])/dy;
+        der[col*ny + ny-2] = (-1.0/60*var[col*ny+ny-5] + 3.0/20.0*var[col*ny+ny-4] - 3.0/4.0*var[col*ny+ny-3] + 3.0/4.0*var[col*ny+ny-1] - 3.0/20.0*var[col*ny] + 1.0/60.0*var[col*ny+1])/dy;
+        der[col*ny + ny-1] = (-1.0/60*var[col*ny+ny-4] + 3.0/20.0*var[col*ny+ny-3] - 3.0/4.0*var[col*ny+ny-2] + 3.0/4.0*var[col*ny] - 3.0/20.0*var[col*ny+1] + 1.0/60.0*var[col*ny+2])/dy;
 
         // elements between first 3 and last 3 elements
         for (int row = 3; row < ny-3; row++)
         {
-            der[col*ny + row] = (-1/60*var[col*ny+row-3] + 3/20*var[col*ny+row-2] - 3/4*var[col*ny+row-1] + 3/4*var[col*ny+row+1] - 3/20*var[col*ny+row+2] + 1/60*var[col*ny+row+3])/dy;
+            der[col*ny + row] = (-1.0/60.0*var[col*ny+row-3] + 3.0/20.0*var[col*ny+row-2] - 3.0/4.0*var[col*ny+row-1] + 3.0/4.0*var[col*ny+row+1] - 3.0/20.0*var[col*ny+row+2] + 1.0/60.0*var[col*ny+row+3])/dy;
         }
     }
 }
@@ -399,15 +414,19 @@ void ShallowWater::FileOutput() {
 
     out.precision(5);
 
+    double x, y;
+
     for (int row = 0; row < ny; row++)
     {
         for (int col = 0; col < nx; col++)
         {
+            x = x0 + col*dx;
+            y = y0 + row*dy;
             // Output x and y location
-            out << setw(4) << "x" << col << setw(4) << " y" << row << " ";
+            out << x << " " << y << " ";
 
             // Output variables
-            out << u << " " << v << " " << h << endl;
+            out << u[col*ny + row] << " " << v[col*ny + row] << " " << h[col*ny + row] << endl;
         }
 
         // Output empty line after column
@@ -460,5 +479,9 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
+    // Create solver object
     ShallowWater solver = ShallowWater(dT, T, Nx, Ny, ic);
+
+    // Solve system
+    solver.TimeIntegrate();
 }
